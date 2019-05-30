3b6df097c83362137fee8ded5f46fb2536a4e27b
Håvard T <35465108+havardt@users.noreply.github.com>
2019-05-30 22:33:17 +0200

    Added unit tests for letter check

---
+++ source/EzPasswordValidator.Tests/LetterCheckTest.cs
@@ -0,0 +1,34 @@
+﻿using EzPasswordValidator.Checks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EzPasswordValidator.Tests
+{
+    [TestClass]
+    public class LetterCheckTest
+    {
+        private LetterCheck _check;
+
+        [TestInitialize]
+        public void Setup() => _check = new LetterCheck();
+
+        [DataRow("A12345")]
+        [DataRow("12345A")]
+        [DataRow("123A45")]
+        [DataTestMethod]
+        public void WhenPasswordContainsExactlyOneLetterThenPasswordIsValid(string validPsw) => 
+            Assert.IsTrue(_check.Execute(validPsw));
+
+        [DataRow("ABC")]
+        [DataRow("bbb")]
+        [DataRow("aBc")]
+        [DataTestMethod]
+        public void WhenPasswordContainsOnlyLettersThenPasswordIsValid(string validPsw) =>
+            Assert.IsTrue(_check.Execute(validPsw));
+
+        [DataRow("123456")]
+        [DataRow("///23))")]
+        [DataTestMethod]
+        public void WhenPasswordContainsNoLettersThenPasswordIsInvalid(string invalidPsw) =>
+            Assert.IsFalse(_check.Execute(invalidPsw));
+    }
+}
