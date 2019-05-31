df1b903b53acb5139d6c0028b5878f129427e195
Håvard T <35465108+havardt@users.noreply.github.com>
2019-05-31 14:52:39 +0200

    Added unit tests for letter sequence check

---
+++ source/EzPasswordValidator.Tests/LetterSequenceCheckTest.cs
@@ -0,0 +1,38 @@
+﻿using EzPasswordValidator.Checks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EzPasswordValidator.Tests
+{
+    [TestClass]
+    public class LetterSequenceCheckTest
+    {
+        private LetterSequenceCheck _check;
+
+        [TestInitialize]
+        public void Setup() => _check = new LetterSequenceCheck();
+
+        [DataRow("abc")]
+        [DataRow("testabc")]
+        [DataRow("xyz")]
+        [DataRow("xyztest")]
+        [DataTestMethod]
+        public void WhenPasswordContainsMostCommonThreeLetterSequencesThenPasswordIsInvalid(string psw) => 
+            Assert.IsFalse(_check.Execute(psw));
+
+        [TestMethod]
+        public void WhenPasswordContainsFourLetterSequenceThenPasswordIsInvalid()
+        {
+            const string invalidPsw = "defg";
+
+            Assert.IsFalse(_check.Execute(invalidPsw));
+        }
+
+        [TestMethod]
+        public void WhenPasswordHasNoLetterSequenceThenPasswordIsValid()
+        {
+            const string validPsw = "valid";
+
+            Assert.IsTrue(_check.Execute(validPsw));
+        }
+    }
+}
