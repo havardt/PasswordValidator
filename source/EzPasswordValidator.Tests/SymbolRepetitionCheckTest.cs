830d7f5bd69bdae276d4b20d596d2dc9e21bfe29
Håvard T <35465108+havardt@users.noreply.github.com>
2019-06-01 15:49:39 +0200

    Added unit tests for symbol repetition check

---
+++ source/EzPasswordValidator.Tests/SymbolRepetitionCheckTest.cs
@@ -0,0 +1,39 @@
+﻿using EzPasswordValidator.Checks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EzPasswordValidator.Tests
+{
+    [TestClass]
+    public class SymbolRepetitionCheckTest
+    {
+        private SymbolRepetitionCheck _check;
+
+        [TestInitialize]
+        public void Setup() => _check = new SymbolRepetitionCheck();
+
+        [TestMethod]
+        public void WhenPasswordContainsNoSymbolsThenPasswordIsValid()
+        {
+            const string psw = "test";
+
+            Assert.IsTrue(_check.Execute(psw));
+        }
+
+        [DataRow("test//")]
+        [DataRow("//test")]
+        [DataRow("te//st")]
+        [DataTestMethod]
+        public void WhenPasswordContainsSymbolRepetitionOfLengthTwoThenPasswordIsValid(string psw) => 
+            Assert.IsTrue(_check.Execute(psw));
+
+        [DataRow("test///")]
+        [DataRow("///test")]
+        [DataRow("te///st")]
+        [DataRow("test#####")]
+        [DataRow("#####test")]
+        [DataRow("te#####st")]
+        [DataTestMethod]
+        public void WhenPasswordContainsSymbolRepetitionOfLengthThreeOrLongerThenPasswordIsValid(string psw) => 
+            Assert.IsFalse(_check.Execute(psw));
+    }
+}
